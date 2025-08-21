a91e417109ce474834ca72c6ee2bbc584496f40b
ahad-jiva <ahadjiva15@gmail.com>
2025-08-20 22:51:06 -0700

    gpu memory indexing and csv writing fixed

--- core/cpu_mc_bsm.cpp
+++ core/cpu_mc_bsm.cpp
@@ -7,62 +7,63 @@
 
 void monte_carlo_eu_call(double S0, double K, double T, double r, double sigma, int n, int steps) {
 
-  // random number setup
-  std::random_device rd{};
-  std::mt19937 gen{rd()};
+	// random number setup
+	std::random_device rd{};
+	std::mt19937 gen{rd()};
 
-  double payoff_sum = 0.0;
+  	double payoff_sum = 0.0;
 
-  auto start_time = std::chrono::high_resolution_clock::now();
+  	std::normal_distribution<double> normal(0.0, 1.0);
 
-  std::normal_distribution<double> normal(0.0, 1.0);
-
-  double dt = T / steps;
+  	double dt = T / steps;
   
-  std::vector<std::vector<double>> paths(n, std::vector<double>(steps + 1, 0.0));
+  	std::vector<std::vector<double>> paths(n, std::vector<double>(steps + 1, 0.0));
 
-  // main loop
-  for (int i = 0; i < n; i++) {
-    paths[i][0] = S0;
-    for (int j = 1; j <= steps; j++){
-      double Z = normal(gen);
-      paths[i][j] = paths[i][j-1] * exp((r - 0.5 * sigma * sigma) * dt + sigma * sqrt(dt) * Z);
-    }
-  }
+	auto start_time = std::chrono::high_resolution_clock::now();
 
-  // write to csv
-  std::ofstream file("bsm_paths.csv");
+	// main loop
+	for (int i = 0; i < n; i++) {
+		paths[i][0] = S0;
+		for (int j = 1; j <= steps; j++){
+		double Z = normal(gen);
+		paths[i][j] = paths[i][j-1] * exp((r - 0.5 * sigma * sigma) * dt + sigma * sqrt(dt) * Z);
+		}
+	}
 
-  // add header info for lognormal calculation in graphing
-  file << S0 << "," << T << "," << r << "," << sigma << "\n";
+	auto end_time = std::chrono::high_resolution_clock::now();
 
-  for (int j = 0; j <= steps; j++){
-    for (int i = 0; i < n; i++){
-      file << paths[i][j];
-      if (i != n - 1) file << ",";
-    }
-    file << "\n"; 
-  }
-  file.close();
+	// write to csv
+	std::ofstream file("bsm_paths.csv");
 
-  std::cout << "Saved " << n << " paths with " << steps << " steps to 'paths.csv'" << std::endl;
+	// add header info for lognormal calculation in graphing
+	file << S0 << "," << T << "," << r << "," << sigma << "\n";
 
-  for (int i = 0; i < n; i++){
-    double S_T = paths[i][steps];
-    double payoff = std::max(S_T - K, 0.0);
-    payoff_sum += payoff;
-  }
+	for (int j = 0; j <= steps; j++){
+		for (int i = 0; i < n; i++){
+		file << paths[i][j];
+		if (i != n - 1) file << ",";
+		}
+		file << "\n"; 
+	}
+	file.close();
 
-  // averaging and discounting
-  double avg_payoff_discounted = (payoff_sum / n) * exp(-r * T);
-  
-  auto end_time = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration<double>(end_time - start_time);
-  
-  std::cout << "Computation took " << duration.count() << " seconds.\n";
-  std::cout << "Option is worth: " << avg_payoff_discounted << std::endl;
+	std::cout << "Saved " << n << " paths with " << steps << " steps to 'paths.csv'" << std::endl;
+
+	for (int i = 0; i < n; i++){
+		double S_T = paths[i][steps];
+		double payoff = std::max(S_T - K, 0.0);
+		payoff_sum += payoff;
+	}
+
+	// averaging and discounting
+	double avg_payoff_discounted = (payoff_sum / n) * exp(-r * T);
+
+	auto duration = std::chrono::duration<double>(end_time - start_time);
+	
+	std::cout << "Computation took " << duration.count() << " seconds.\n";
+	std::cout << "Option is worth: " << avg_payoff_discounted << std::endl;
 }
 
 int main() {
-    monte_carlo_eu_call(100, 150, 10, 0.05, 0.1, 2000, 100);
+    monte_carlo_eu_call(100, 120, 10, 0.05, 0.1, 20000, 500);
 }
--- core/gpu_mc_bsm.cu
+++ core/gpu_mc_bsm.cu
@@ -1,29 +1,37 @@
 #include <cmath>
 #include <chrono>
 #include <iostream>
+#include <fstream>
 #include <random>
 #include <curand_kernel.h>
 
-__global__ void monte_carlo_eu_call(double *payoffs, double S0, double K, double T, double r, double sigma, unsigned int seed, int n) {
+__global__ void monte_carlo_eu_call(double *paths, double S0, double K, double T, double r, double sigma, unsigned int seed, int n_paths, int n_steps) {
     // All parameters will be the same for each thread
     // Only the random normal sample will be per-thread
-    int idx = threadIdx.x;
-    if (idx >= n) return;
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= n_paths) return;
 
     curandState state;
     curand_init(seed, idx, 0, &state);
-    double Z = curand_normal_double(&state);
 
-    double S_T = S0 * exp((r - 0.5 * sigma * sigma) * T + sigma * sqrt(T) * Z);
+    double dt = T / (n_steps - 1);
+    double S = S0;
 
-    payoffs[idx] = max(S_T - K, 0.0);
+    paths[idx * n_steps + 0] = S;
+
+    for (int step = 1; step < n_steps; step++){
+        double Z = curand_normal_double(&state);
+        S *= exp((r - 0.5 * sigma * sigma) * dt + sigma * sqrt(dt) * Z);
+        paths[idx * n_steps + step] = S;
+    }
 
 }
 
 int main(){
 
     // set up parameters
-    int n_paths = 100000000;
+    int n_paths = 20000;
+    int n_steps = 500;
     double S0 = 100;
     double K = 150;
     double T = 10;
@@ -31,27 +39,41 @@ int main(){
     double sigma = 0.1;
 
     // memory
-    double *payoffs;
-    cudaMalloc(&payoffs, n_paths * sizeof(double));
+    double *paths;
+    cudaMalloc(&paths, n_paths * n_steps * sizeof(double));
+
+    // kernel config
+    int threads_per_block = 256;
+    int num_blocks = (n_paths + threads_per_block - 1) / threads_per_block;
 
     auto start_time = std::chrono::high_resolution_clock::now();
-    monte_carlo_eu_call<<<1, 256>>>(payoffs, S0, K, T, r, sigma, time(NULL), n_paths);
+    monte_carlo_eu_call<<<num_blocks, threads_per_block>>>(paths, S0, K, T, r, sigma, time(NULL), n_paths, n_steps);
+
     cudaDeviceSynchronize();
     auto end_time = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration<double>(end_time - start_time);
-
-    std::vector<double> h_payoffs(n_paths);
-    cudaMemcpy(h_payoffs.data(), payoffs, n_paths * sizeof(double), cudaMemcpyDeviceToHost);
-
-    double payoff_sum = 0.0;
-    for (int i = 0; i < n_paths; i++){
-        payoff_sum += h_payoffs[i];
-    }
-    double option_price = (payoff_sum / n_paths) * exp(-r * T);
-
-    cudaFree(payoffs);
     std::cout << "Computation on GPU took " << duration.count() << " seconds.\n";
-    std::cout << "Option is worth: " << option_price << std::endl;
+
+    // copy results to host
+    double *d_paths = (double *)malloc(n_paths * n_steps * sizeof(double));
+    cudaMemcpy(d_paths, paths, n_paths * n_steps * sizeof(double), cudaMemcpyDeviceToHost);
+    std::cout << "Data successfully copied back to host memory" << std::endl;
+
+    cudaFree(paths);
+
+    // write to csv
+    std::ofstream file("bsm_paths.csv");
+    file << S0 << "," << T << "," << r << "," << sigma << "\n";
+
+    for (int j = 0; j < n_steps; j++) {
+        for (int i = 0; i < n_paths; i++) {
+            file << d_paths[i * n_steps + j];
+            if (i != n_paths - 1) file << ",";
+        }
+        file << "\n";
+}
+file.close();
+    std::cout << "Paths written to bsm_paths.csv\n";
 
 }
